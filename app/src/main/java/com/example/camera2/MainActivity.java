6f63307543c249300411bd25bd7a17818520c118
Saakshi <saakshi.c18@bmsce.ac.in>
2020-09-08 19:58:27 +0530

    hi

--- app/src/main/java/com/example/camera2/MainActivity.java
+++ app/src/main/java/com/example/camera2/MainActivity.java
@@ -8,43 +8,144 @@ import androidx.camera.core.CameraSelector;
 import androidx.camera.core.CameraX;
 import androidx.camera.core.CameraXConfig;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity implements CameraXConfig.Provider{
 
+    private Executor executor = Executors.newSingleThreadExecutor();
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private PreviewView previewView;
+    private int REQUEST_CODE_PERMISSIONS = 1001;
+    private Button button;
+    //private StorageReference mStorageRef;
 
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA",
+                                                               "android.permission.WRITE_EXTERNAL_STORAGE",
+                                                                "android.permission.READ_EXTERNAL_STORAGE"};
+    ImageCapture imageCapture = new ImageCapture.Builder().build();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //mStorageRef = FirebaseStorage.getInstance().getReference();
+        button=findViewById(R.id.button);
         previewView=findViewById(R.id.previewView);
-        ImageCapture imageCapture =
-                new ImageCapture.Builder()
-                        .build();
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                bindPreview(cameraProvider);
-            } catch (ExecutionException | InterruptedException e) {
-                // No errors need to be handled for this Future.
-                // This should never be reached.
-            }
-        }, ContextCompat.getMainExecutor(this));
+        boolean permissions=allPermissionsGranted();
+            cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+            cameraProviderFuture.addListener(() -> {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    // No errors need to be handled for this Future.
+                    // This should never be reached.
+                }
+            }, ContextCompat.getMainExecutor(this));
+//
+//        else{
+//            Toast.makeText(this,"Permissions not granted",Toast.LENGTH_SHORT);
+//        }
 
-    }
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
+//                File file = new File(getBatchDirectoryName(), mDateFormat.format(new Date())+ ".jpg");
+//
+//                ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
+//                imageCapture.takePicture(outputFileOptions, executor, new ImageCapture.OnImageSavedCallback () {
+//                    @Override
+//                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+//                        new Handler().post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                Toast.makeText(MainActivity.this, "Image Saved successfully", Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                    }
+//                    @Override
+//                    public void onError(@NonNull ImageCaptureException error) {
+//                        error.printStackTrace();
+//                    }
+//                });
+//            }
+//        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
+                //File file = new File(getBatchDirectoryName(), mDateFormat.format(new Date())+ ".jpg");
+                //File file = new File("data/data//test.jpg");
+
+                //file creation
+                String root = Environment.getExternalStorageDirectory().toString();
+                File myDir = new File(root + "/iku/images/sent_images");
+
+                    myDir.mkdirs();
+                
+                Random generator = new Random();
+                int n = 10000;
+                n = generator.nextInt(n);
+                String fname = "Image-"+ n +".jpg";
+                File file = new File (myDir, fname);
+                if (file.exists ())
+                    file.delete ();
+                if (!file.exists()) {
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                ImageCapture.OutputFileOptions outputFileOptions =
+                        new ImageCapture.OutputFileOptions.Builder(file).build();
+                imageCapture.takePicture(outputFileOptions, executor,
+                        new ImageCapture.OnImageSavedCallback() {
+                            @Override
+                            public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                                Log.i("Image ","Captured and saved");
+                            }
+                            @Override
+                            public void onError(ImageCaptureException error) {
+                                Log.i("Image ","Error");
+                                Log.i("Error",error.getMessage());
+                            }
+                        });
+            }
+
+        });
+      }
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
         Preview preview = new Preview.Builder()
                 .build();
@@ -55,7 +156,29 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
 
         preview.setSurfaceProvider(previewView.createSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview,imageCapture);
+
+    }
+
+    private boolean allPermissionsGranted(){
+
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSIONS);
+                return false;
+            }
+        }
+        return true;
+    }
+    public String getBatchDirectoryName() {
+
+        String app_folder_path = "";
+        app_folder_path = Environment.getExternalStorageDirectory().toString() + "/images";
+        File dir = new File(app_folder_path);
+        if (!dir.exists() && !dir.mkdirs()) {
+                Log.i("Error ","creating directory");
+        }
+        return app_folder_path;
     }
 
     @NonNull

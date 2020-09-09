8b6681bb41fa47e5bfce70a00273612468fc8f24
Saakshi <saakshi.c18@bmsce.ac.in>
2020-09-09 20:19:21 +0530

    Hello

--- app/src/main/java/com/example/camera2/MainActivity.java
+++ app/src/main/java/com/example/camera2/MainActivity.java
@@ -17,18 +17,32 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -38,6 +52,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import static android.Manifest.permission.CAMERA;
+
 public class MainActivity extends AppCompatActivity implements CameraXConfig.Provider{
 
     private Executor executor = Executors.newSingleThreadExecutor();
@@ -45,9 +61,11 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
     private PreviewView previewView;
     private int REQUEST_CODE_PERMISSIONS = 1001;
     private Button button;
-    //private StorageReference mStorageRef;
+    private Uri mImageUri;
+    private Uri mainImageUri;
+    private StorageReference mStorageRef;
 
-    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA",
+    private final String[] mPermission= new String[]{"android.permission.CAMERA",
                                                                "android.permission.WRITE_EXTERNAL_STORAGE",
                                                                 "android.permission.READ_EXTERNAL_STORAGE"};
     ImageCapture imageCapture = new ImageCapture.Builder().build();
@@ -55,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //mStorageRef = FirebaseStorage.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         button=findViewById(R.id.button);
         previewView=findViewById(R.id.previewView);
         boolean permissions=allPermissionsGranted();
@@ -109,10 +127,8 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
 
                 //file creation
                 String root = Environment.getExternalStorageDirectory().toString();
-                File myDir = new File(root + "/iku/images/sent_images");
-
-                    myDir.mkdirs();
-                
+                File myDir = new File(root + "/images");
+                myDir.mkdirs();
                 Random generator = new Random();
                 int n = 10000;
                 n = generator.nextInt(n);
@@ -134,7 +150,36 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
                         new ImageCapture.OnImageSavedCallback() {
                             @Override
                             public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
-                                Log.i("Image ","Captured and saved");
+
+                                Log.i("Image ","Captured and saved "+fname);
+                                Log.i("file path:",file.getAbsolutePath());
+                                Uri mImageUri = Uri.fromFile(new File(file.getAbsolutePath()));
+                                Log.i("Hey","Image crossed");
+                                Log.i("Uri:",mImageUri.toString());
+                                if(mImageUri!=null)
+                                {
+                                    final StorageReference imageRef = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mImageUri));
+                                    UploadTask uploadTask = imageRef.putFile(mImageUri);
+                                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception exception) {
+                                           Log.i("Error: ","Failed");
+                                        }
+                                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                            // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                                            // ...
+                                            Log.i("Success: ","Success");
+                                        }
+                                    });
+                                }
+                                else{
+                                    Log.i("ImageUri","is null"+mImageUri);
+                                }
+
+//                                StorageReference riversRef = mStorageRef.child("images/"+file.getLastPathSegment());
+//                                UploadTask uploadTask = riversRef.putFile(file);
                             }
                             @Override
                             public void onError(ImageCaptureException error) {
@@ -160,13 +205,103 @@ public class MainActivity extends AppCompatActivity implements CameraXConfig.Pro
 
     }
 
+
+
+   // Register observers to listen for when the download is done or if it fails
+   //  uploadTask.addOnFailureListener(new OnFailureListener() {
+   //        @Override
+   //        public void onFailure(@NonNull Exception exception) {
+   //            // Handle unsuccessful uploads
+   //        }
+   //    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+   //        @Override
+   //        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+   //            // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+   //            // ...
+   //        }
+    //    });
+
+    private void uploadFile() throws FileNotFoundException {
+        if (mImageUri != null) {
+            Bitmap imageSelected = decodeUri(this, mImageUri, 300);
+            if (imageSelected != null)
+                mainImageUri = getImageUri(this, imageSelected);
+            if (mainImageUri != null) {
+                final StorageReference imageRef = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mainImageUri));
+                UploadTask uploadTask = imageRef.putFile(mainImageUri);
+                //SaveImage(imageSelected);
+            }
+
+        } else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_LONG).show();
+        }
+    }
+            private String getFileExtension(Uri uri) {
+            if (uri != null) {
+                ContentResolver cR = getContentResolver();
+                MimeTypeMap mime = MimeTypeMap.getSingleton();
+                return mime.getExtensionFromMimeType(cR.getType(uri));
+            } else return null;
+        }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "images", null);
+
+        if (path != null) return Uri.parse(path);
+        else return null;
+    }
+    public static Bitmap decodeUri(Context c, Uri uri, final int requiredSize) throws FileNotFoundException {
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(c.getContentResolver().openInputStream(uri), null, o);
+
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+
+        while (true) {
+            if (width_tmp / 2 < requiredSize || height_tmp / 2 < requiredSize)
+                break;
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        return BitmapFactory.decodeStream(c.getContentResolver().openInputStream(uri), null, o2);
+    }
+
     private boolean allPermissionsGranted(){
 
-        for(String permission : REQUIRED_PERMISSIONS){
-            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSIONS);
-                return false;
+//        for(String permission : REQUIRED_PERMISSIONS){
+//            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+//                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSIONS);
+//                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSIONS);
+//                return false;
+//            }
+//        }
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission[0])
+                    != PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(this, mPermission[1])
+                            != PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(this, mPermission[2])
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSIONS);
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSIONS);
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSIONS);
+
+                // If any permission aboe not allowed by user, this condition will execute every tim, else your else part will work
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return true;
     }
